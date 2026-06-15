a48934bd86ddf78a72a6f9a1649530124fa0fb89
Emanuel Gonzalez <eg554848@gmail.coml>
2026-06-14 18:41:59 -0600

    chore: protect roles route

--- src/modules/roles/role.routes.ts
+++ src/modules/roles/role.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { getRoles } from "./role.controller";
+import { authMiddleware } from "../../middlewares/auth.middlewar";
+import { authorizeRoles } from "../../middlewares/role.middleware";
 
 
 const router = Router()
 
 
 
-router.get("/roles", getRoles)
+router.get("/roles", authMiddleware, authorizeRoles("owner", "admin"), getRoles)
 
 
 
